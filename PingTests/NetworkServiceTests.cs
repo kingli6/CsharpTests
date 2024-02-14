93185165313995f5c24ebc4f3802fd7d7a370748
Joanthan Fisher <jonathan.fshr@gmail.com>
2024-02-14 22:04:03 +0100

    init

---
+++ PingTests/NetworkServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using NetworkUtility.Ping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkUtility.Test.PingTests
+{
+    public class NetworkServiceTests
+    {
+        [Fact]
+        public void NetworkService_SendPing_ReturnString()
+        {
+            //Arrange
+            NetworkService pingService = new();
+            //Act
+            var result = pingService.SendPing();
+            //Assert
+            result.Should().NotBeNullOrWhiteSpace();
+            result.Should().Be("Success: Ping Sent!");
+            result.Should().Contain("Success", Exactly.Once());
+        }
+    }
+}
